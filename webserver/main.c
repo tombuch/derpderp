b81dbd02a69dd0e53084f94aef62e24fc3366c76
Thomas BUCHARD <buchardt@hevea21.iut-infobio.priv.univ-lille1.fr>
2016-09-20 14:04:36 +0200

    Add part 3

--- webserver/main.c
+++ webserver/main.c
@@ -2,42 +2,39 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <fnctl.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "socket.h"
 
-
-void	bienvenue(int socket_client){
+void	welcome_client(int socket_client){
   char welcome[1024];
   int fd;
   int n = 0;
   
-  fd = open("ressourse/Welcome", O_RONLY);
+  fd = open("ressources/Welcome", O_RDONLY);
   if (fd == -1)
+    {
+      perror("open Welcome\n");
     return;
-  while ((n = read(fd, welcome, 1024) > 0){
-      
+    }
+  while ((n = read(fd, welcome, 1024)) > 0){
+      write(socket_client, &welcome, n);
     }
 }
 
-int	main(int argc, char **argv){
-
-  /*  if (argc > 1 && strncmp(argv[1], "-advice", 7) == 0){
-    printf("Don't panic!\n");
-    return 42;
-  }
-  printf("Need an advice?\n");*/
-
+int	main(){
   int socket_serveur;
   int socket_client;
   
   socket_serveur = creer_serveur(8080);
   if (socket_serveur == -1)
     return -1;
-  socket_client = accept(socket_serveur, null,null);
+  socket_client = accept(socket_serveur, NULL,NULL);
   if (socket_client == -1){
     perror("socket_client");
     return -1;
   }
-  
-  
+  welcome_client(socket_client);
   return 0;
 }
